c11b4a287bb1d588d1113a3e92dea509b76b86ef
ingalls <nick@ingalls.ca>
2024-08-05 11:51:51 -0700

    Add CoT Types build

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import CoT from './lib/cot.js';
 export * as Feature from './lib/feature.js'
 export * as Types from './lib/types.js'
+export * as CoTTypes from './lib/cot-types.js'
 export * from './lib/chat.js'
 export * from './lib/fileshare.js'
 export * from './lib/data-package.js'
