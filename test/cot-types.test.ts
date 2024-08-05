6d177a2981d13cb864d73e897157ad20efd54c6a
ingalls <nick@ingalls.ca>
2024-08-05 11:53:10 -0700

    Built Errors

--- test/cot-types.test.ts
+++ test/cot-types.test.ts
@@ -2,7 +2,7 @@ import { CoTTypes } from '../index.js'
 import test from 'tape';
 
 test(`CoTTypes Parsing`, async (t) => {
-    const types = await CoTTypes.CoTTypes.load();
+    await CoTTypes.CoTTypes.load();
 
     t.end();
 });
