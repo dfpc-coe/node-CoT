4ec7f0fc803157791e6bb2f506b37f67efbc7ffd
ingalls <nick@ingalls.ca>
2024-12-04 06:00:00 -0700

    Repeatable Proto

--- test/reversal-proto.test.ts
+++ test/reversal-proto.test.ts
@@ -17,3 +17,44 @@ for (const fixturename of await fs.readdir(new URL('./fixtures/', import.meta.ur
         t.end();
     });
 }
+
+// Ref: https://github.com/dfpc-coe/node-CoT/issues/55
+test('Protobuf Multiple Calls', (t) => {
+    const cot = new CoT({
+        event: {
+            _attributes: {
+                version: '2.0',
+                uid: 'ebbf42a7-ea71-43a1-baf6-e259c3d115bf',
+                type: 'u-rb-a',
+                how: 'h-e',
+                time: '2024-08-30T22:28:02Z',
+                start: '2024-08-30T22:28:02Z',
+                stale: '2024-08-31T22:28:02Z',
+                access: 'Undefined',
+            },
+            point: {
+                _attributes: {
+                    lat: '39.0981196',
+                    lon: '-108.7395013',
+                    hae: '0.0',
+                    ce: '9999999.0',
+                    le: '9999999.0',
+                },
+            },
+            detail: {
+                contact: {
+                    _attributes: {
+                        callsign: 'sign',
+                    },
+                },
+            },
+        },
+    })
+
+    const cot2 = CoT.from_proto(cot.to_proto())
+    t.deepEqual(cot2.raw.event.detail?.contact?._attributes.callsign, 'sign')
+    const cot3 = CoT.from_proto(cot.to_proto())
+    t.deepEqual(cot3.raw.event.detail?.contact?._attributes.callsign, 'sign')
+
+    t.end();
+});
