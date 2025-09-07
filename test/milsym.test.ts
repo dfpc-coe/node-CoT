984f3437fb9b7ffa2c678c61a343eefde14ec65e
ingalls <nick@ingalls.ca>
2025-09-07 08:34:46 -0600

    Add Augment Test

--- test/milsym.test.ts
+++ test/milsym.test.ts
@@ -48,3 +48,31 @@ test('Parse Creator', (t) => {
 
     t.end();
 });
+
+test('MilSym Augmentation', (t) => {
+    const cot = CoTParser.from_xml(`
+        <event
+            version="2.0"
+            type="a-n-G"
+            how="h-g-i-g-o"
+            uid="a471cec4-4d18-41b4-a1c5-d79e52b5a678"
+            time="2025-04-24T15:23:11.088Z"
+            start="2025-04-24T15:23:11.088Z"
+            stale="2026-04-24T15:23:11.088Z"
+            access="Undefined"
+        >
+            <point lat="39.0664644" lon="-108.4328957" hae="1402.385" ce="9999999.0" le="9999999.0"/>
+            <detail></detail>
+        </event>
+    `, {
+        milsym: {
+            augment: true
+        }
+    });
+
+    t.deepEquals(cot.raw.event.detail!.__milsym, {
+        _attributes: { id: 'SNGP-----------' },
+    });
+
+    t.end();
+});
