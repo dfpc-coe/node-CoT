9ad22fc518466779c7188986a0308f894d196125
ingalls <nick@ingalls.ca>
2026-02-04 11:08:35 -0700

    Update Additional Properties

--- lib/types/basemap.ts
+++ lib/types/basemap.ts
@@ -1,32 +1,44 @@
 import { Type } from '@sinclair/typebox';
 
+/**
+ * Type Schema for Basemap Map Source Configuration
+ * Compatible with Mobile Atlas Creator (MOBAC) Custom XML Map Sources
+ * @link https://mobac.sourceforge.io/wiki/index.php/Custom_XML_Map_Sources
+ */
+
 export const BasemapMapSource = Type.Object({
     name: Type.Object({
         _text: Type.String()
-    }, { description: 'Name of the Basemap' }),
+    }, { description: 'The name of the map source' }),
     minZoom: Type.Object({
         _text: Type.Integer()
-    }, { description: 'Minimum Zoom Level' }),
+    }, { description: 'The minimum zoom level provided by the map source' }),
     maxZoom: Type.Object({
         _text: Type.Integer()
-    }, { description: 'Maximum Zoom Level' }),
+    }, { description: 'The maximum zoom level provided by the map source' }),
     tileType: Type.Object({
         _text: Type.String()
-    }, { description: 'Tile Format/Type' }),
+    }, { description: 'The image type provided by the map source' }),
     tileUpdate: Type.Optional(Type.Object({
         _text: Type.String({
             default: 'None'
         })
-    }, { description: 'Tile Update Strategy' })),
+    }, { description: 'The server capabilities for conditional downloading of new/updated tiles' })),
     url: Type.Optional(Type.Object({
         _text: Type.String()
-    }, { description: 'Tile Server URL Template' })),
+    }, { description: 'The URL for the tiles of the map source' })),
+    invertYCoordinate: Type.Optional(Type.Object({
+        _text: Type.String()
+    }, { description: 'Inverts the y coordinate so that it starts south' })),
     backgroundColor: Type.Optional(Type.Object({
         _text: Type.String()
-    }, { description: 'Map Background Color' })),
+    }, { description: 'The background color of a map' })),
+    ignoreErrors: Type.Optional(Type.Object({
+        _text: Type.String()
+    }, { description: 'Handling of missing tiles' })),
     serverParts: Type.Optional(Type.Object({
         _text: Type.String()
-    }, { description: 'A, B, C, prefix used in some tile servers' }))
+    }, { description: 'Use multiple servers for the map source' }))
 })
 
 export default Type.Object({
